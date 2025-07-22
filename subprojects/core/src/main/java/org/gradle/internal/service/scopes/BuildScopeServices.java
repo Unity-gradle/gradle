f3af82e8033ac84b1fb3bb4fcbf504c8d5d04812
Justin Van Dort <jvandort@gradle.com>
2025-07-22 11:05:43 -0400

    Create interface and default implementation for ManagedObjectRegistry

--- subprojects/core/src/main/java/org/gradle/internal/service/scopes/BuildScopeServices.java
+++ subprojects/core/src/main/java/org/gradle/internal/service/scopes/BuildScopeServices.java
@@ -200,6 +200,7 @@ import org.gradle.internal.file.RelativeFilePathResolver;
 import org.gradle.internal.file.Stat;
 import org.gradle.internal.hash.ClassLoaderHierarchyHasher;
 import org.gradle.internal.instantiation.InstantiatorFactory;
+import org.gradle.internal.instantiation.managed.DefaultManagedObjectRegistry;
 import org.gradle.internal.instantiation.managed.ManagedObjectRegistry;
 import org.gradle.internal.instrumentation.reporting.PropertyUpgradeReportConfig;
 import org.gradle.internal.invocation.DefaultBuildInvocationDetails;
@@ -282,7 +283,7 @@ public class BuildScopeServices implements ServiceRegistrationProvider {
 
     @Provides
     ManagedObjectRegistry decorateManagedObjectRegistry(ManagedObjectRegistry parent) {
-        return new ManagedObjectRegistry(parent);
+        return new DefaultManagedObjectRegistry(parent);
     }
 
     @Provides
